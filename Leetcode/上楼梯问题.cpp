ddba006adb17c2b495718e0735226a4e834e1c50
wsy081414 <953659912@qq.com>
2017-04-22 18:20:08 +0800

    4-22

---
+++ Leetcode/上楼梯问题.cpp
@@ -0,0 +1,19 @@
+
+
+//说给你n阶台阶，对于这n阶台阶我们有两种上法，要么可以一次一阶，要么一次两阶。
+//求得n阶台阶有多少种方法可以爬上去。
+//一种斐波那契数列的变形的解法。
+//可以高效的求得
+//动态规划
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int >data(n+1,-1);
+        data[0] = 1;data[1] = 1;
+        for(int i = 2;i <= n;i++)
+        {
+            data[i] = data[i-1]+data[i+2];
+        }
+        return data[n];
+    }
+};
--- SortingAlgorithm/堆排序.cpp
+++ SortingAlgorithm/堆排序.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 //当你需要降序的时候，你需要小堆，当你需要升序的时候，你需要大堆
 //时间复杂度：N*logN
 //建堆加上调整的：
@@ -16,12 +15,14 @@ void AdjustDown(T* arr, size_t root,size_t count)
 	assert(arr);
 	size_t parent = root;
 	size_t child = parent * 2 + 1;
-	while (parent<count)
+	while (paren < count)
 	{
+		//找出两个孩子当中最大的下标
 		if (child + 1 < count&&arr[child + 1]<arr[child])
 		{
 			++child;
 		}
+		//中出以后把孩子和父亲进行交换，然后向下进行调整。
 		if (child<count&&arr[child] < arr[parent])
 		{
 			std::swap(arr[child], arr[parent]);
@@ -35,15 +36,23 @@ void AdjustDown(T* arr, size_t root,size_t count)
 	}
 
 }
+
+
 template<typename T>
 void HeapSort(T* arr, size_t size)
 {
 	assert(arr);
+	
+	//建堆
 	for (int i = (size - 2) / 2; i >= 0; i--)
 	{
 		AdjustDown(arr, i,size);
 	}
+	
+	//然后把大堆上面的最大的和最后一个交换，然后从其中进行向下调整，找出最大的。
 	size_t end = size - 1;
+	
+	//
 	while (end > 0)
 	{
 		swap(arr[0], arr[end]);
@@ -57,10 +66,7 @@ void test1()
 {
 	int arr[] = { 1,2,3,4,5,6 };
 	HeapSort(arr, sizeof(arr) / sizeof(arr[0]));
-
-
 }
-
 
 int main()
 {
